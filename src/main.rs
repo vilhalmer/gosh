45ab83bd7a14d4f88eb655ff081c0eddca94434c
wrdoyle <wrdoyle@mtu.edu>
2015-10-08 21:43:20 -0400

    A basic input loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate term;
+use std::io::prelude::*;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut t = term::stdout().unwrap();
+
+    t.fg(term::color::GREEN).unwrap();
+    writeln!(t, "Welcome to gosh!").unwrap();
+    t.reset().unwrap();
+
+    loop {
+        write!(t, ">> ").unwrap();
+        t.flush().unwrap();
+
+        let mut phrase = String::new();
+        io::stdin().read_line(&mut phrase).ok().expect("Failed to read phrase.");
+
+        let phrase = phrase; // No longer mutable.
+
+        let words: Vec<&str> = phrase.split_whitespace().collect();
+
+        writeln!(t, "{:?}", words).unwrap();
+    }
 }
