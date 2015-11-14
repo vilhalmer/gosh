aee8154a9ca38f9b6c51e5fcdebae224e4201d3f
wrdoyle <wrdoyle@mtu.edu>
2015-11-13 22:16:11 -0500

    Switch from term to ansi_term, nicer interface.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-extern crate term;
 use std::io::prelude::*;
 use std::io;
+
+extern crate ansi_term;
+use ansi_term::Colour::*;
 
 mod parser;
 use parser::{ParserResult, ParserError};
 
 fn main() {
-    let mut t = term::stdout().unwrap();
+    let mut out = io::stdout();
 
-    t.fg(term::color::GREEN).unwrap();
-    writeln!(t, "Welcome to gosh!").unwrap();
-    t.reset().unwrap();
+    out.write(Green.bold().paint("Welcome to gosh!\n").to_string().as_bytes());
 
     loop {
-        write!(t, ">> ").unwrap();
-        t.flush().unwrap();
+        out.write(">> ".as_bytes());
+        out.flush();
 
         let mut stanza = String::new();
         io::stdin().read_line(&mut stanza).ok().expect("Failed to read phrase.");
@@ -22,9 +22,11 @@ fn main() {
         let stanza: ParserResult = parser::parse(&stanza);
 
         if let Err(error) = stanza {
-            t.fg(term::color::RED).unwrap();
-            writeln!(t, "{}", error).unwrap();
-            t.reset().unwrap();
-        };
+            out.write(Red.paint(format!("{}\n", error)).to_string().as_bytes());
+        }
+        else if let Ok(stanza) = stanza {
+            out.write(format!("{:?}\n", stanza).as_bytes());
+        }
     }
 }
+
