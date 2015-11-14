e38302bf4ecb84eac2e6c9d89460c982cbf21cff
wrdoyle <wrdoyle@mtu.edu>
2015-11-14 15:17:25 -0500

    out! macro to make printing to the screen nicer
    Not happy with it yet, but I've reached the limit of my current
    knowledge of Rust's macros.

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,42 @@ extern crate ansi_term;
 use ansi_term::Colour::*;
 
 mod parser;
-use parser::{ParserResult, ParserError};
+
+macro_rules! out {
+    ($message:expr) => {{
+        let mut out = io::stdout();
+        out.write($message.to_string().as_bytes()).unwrap();
+        out.flush().unwrap();
+    }};
+
+    ($style:expr, $message:expr) => (out!($style.paint($message)));
+
+    //($message:expr, $($params:expr),+) => (out!(format!($message, $($params),*)));
+
+    //($style:expr, $message:expr, $($params:expr),+) => (out!($style.paint(format!($message, $($params),*))));
+}
 
 fn main() {
-    let mut out = io::stdout();
+    let debug = true;
 
-    out.write(Green.bold().paint("Welcome to gosh!\n").to_string().as_bytes());
+    out!(Green.bold(), "Welcome to gosh!\n");
 
     loop {
-        out.write(">> ".as_bytes());
-        out.flush();
+        out!(">> ");
 
         let mut stanza = String::new();
         io::stdin().read_line(&mut stanza).ok().expect("Failed to read phrase.");
 
-        let stanza: ParserResult = parser::parse(&stanza);
+        let stanza = match parser::parse(&stanza) {
+            Err(error) => {
+                out!(Red, format!("{}\n", error));
+                continue;
+            },
 
-        if let Err(error) = stanza {
-            out.write(Red.paint(format!("{}\n", error)).to_string().as_bytes());
-        }
-        else if let Ok(stanza) = stanza {
-            out.write(format!("{:?}\n", stanza).as_bytes());
-        }
+            Ok(stanza) => stanza,
+        };
+
+        if debug { out!(format!("{:?}\n", stanza)); }
     }
 }
 
